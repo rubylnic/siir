674d70035ab2430cbc3e507409d9f39cb108d48e
Ксения Большакова <ksenia.bolshako@mail.ru>
2021-05-31 18:03:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -56,31 +56,6 @@
 })();
 'use strict';
 (function() {
-  var openBtn = document.querySelector('.header__burger');
-  var closeBtn = document.querySelector('.header__close');
-  var nav = document.querySelector('.nav');
-
-  var openMenu = function(evt) {
-    evt.preventDefault();
-    nav.classList.remove('nav--hidden');
-    nav.classList.add('nav--show');
-    openBtn.classList.add('header__burger--closed');
-    closeBtn.classList.remove('header__close--closed');
-  };
-
-  var closeMenu = function(evt) {
-    evt.preventDefault();
-    nav.classList.add('nav--hidden');
-    nav.classList.remove('nav--show');
-    openBtn.classList.remove('header__burger--closed');
-    closeBtn.classList.add('header__close--closed');
-  };
-
-  openBtn.addEventListener('click', openMenu);
-  closeBtn.addEventListener('click', closeMenu);
-})();
-'use strict';
-(function() {
   //кнопка открытия - <div><a href="#" class="(классы для стилей) open-modal" data-modal="1" и т.д. (data-modal="2" ...)></div>
 
   //сами модалки <section class="modal modal--closed" data-modal-content="1"> и т.д. (data-modal-сontent="2" (соответствует кнопке открытия))>
